edca1d5e6f63ad9b08ebf10e61b09ae320312811
“Smriti1006” <smriti.raina1006@gmail.com>
2021-03-05 16:23:38 +0100

    Initial commit with tuples for optimisation

---
+++ FibonacciOnScreen/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  FibonacciOnScreen
+//
+//  Created by Smriti Raina on 05.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let maxLimit = UInt64.max
+    var fibs: [UInt64] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.fib(UInt64(maxLimit))
+       // print(myarray)
+        // Do any additional setup after loading the view.
+    }
+
+    private func fib(_ n: UInt64) -> [UInt64] {
+
+        var a = UInt64(0)
+        var b = UInt64(1)
+        fibs.append(a)
+        fibs.append(b)
+        guard n > 1 else { return fibs }
+        
+        (2...n).forEach { _ in
+            (a, b) = (a &+ b, a)
+            print(a)
+            fibs.append(a)
+
+        }
+        return fibs
+        
+        //        (2...n).forEach { i in
+//            fibs.append(fibs[Int(UInt64(i) - UInt64(1))] + fibs[Int(UInt64(i) - UInt64(2))])
+//        }
+//        return fibs
+    }
+    
+
+}
+
