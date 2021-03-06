e79f48bf4be343c7949adc738da9aecd3479a636
“Smriti1006” <smriti.raina1006@gmail.com>
2021-03-06 17:11:22 +0100

    Code cleanup,Adding comments for functions

--- FibonacciOnScreen/ViewController.swift
+++ FibonacciOnScreen/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController,UITextViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.fibonacciSeriesTextView.delegate = self
         self.fibonacciSeriesTextView.layoutManager.allowsNonContiguousLayout = false
         self.getFibSeries()
     }
@@ -31,53 +30,61 @@ class ViewController: UIViewController,UITextViewDelegate {
         var result : UInt64 = 0
         var temp : UInt64 = 0
         
-        DispatchQueue.global(qos: .background).async { [self] in
+        DispatchQueue.global(qos: .background).async { [self] in //perform calculation on background
         
         while true {
+            result = fibonacciRecurrsive(index)
             
-            result = fibRecur(index)
+            // Checking whether next element is casuing overflow or not
+            let sum = temp.addingReportingOverflow(result)
             
-            let sum = temp.addingReportingOverflow(result).overflow
-            
-            if sum == true {
+            // Terminate the loop when overflow occurs
+            if sum.overflow == true {
                 print("Overflow occurred at",index)
-                self.updateTextViewWith(nextNum: result)
+                self.updateTextViewWith(nextNum: result) //Update UI when overflow occured
                 break
             }
             
-            if result >= maxIntValue {
+            // Terminate the loop when result is greater than or equal to MaxIntValue.
+            // Example: If MaxIntValue = 10 then it will terminate the loop when result is 8
+            
+            if sum.partialValue >= maxIntValue {
                 print("Max UInt64 value reached!!")
                 break
             }
             
-            self.updateTextViewWith(nextNum: result)
-            
+            self.updateTextViewWith(nextNum: result) //Keep Updating UI till condition meets
+
             index = index + 1
             temp = result
-            usleep(1000)
+            usleep(5000)
         }
         }
     }
-    
+
+    //Updates textview text everytime a new element in series is calculated
     private func updateTextViewWith(nextNum: UInt64){
         DispatchQueue.main.sync { [self] in
-                self.fibonacciSeriesTextView.text = self.fibonacciSeriesTextView.text + "\(nextNum),\n"
+            self.fibonacciSeriesTextView.text = self.fibonacciSeriesTextView.text + "\(nextNum),\n"
+            
             let range = NSMakeRange(self.fibonacciSeriesTextView.text.count, 0);
-            self.fibonacciSeriesTextView.scrollRangeToVisible(range);
-
+            self.fibonacciSeriesTextView.scrollRangeToVisible(range);//For scrolling to bottom so that newest calculated element is visible
             }
-        }
+    }
     
-    
-    private func fibRecur(_ n: Int) -> (UInt64)  {
+    private func fibonacciRecurrsive(_ n: Int) -> (UInt64)  {
  
         guard n > 1 else {
             return UInt64(n)
         }
-        let fibResult: UInt64 = (((fibDict[n-2] != nil) ? fibDict[n-2]! : fibRecur(n-2))) &+ (((fibDict[n-1] != nil) ? fibDict[n-1]! : fibRecur(n-1)))
-       
-        fibDict[n] = fibResult
-        fibDict.removeValue(forKey: n - 3)
+        // Call recurrsive funtion when dictionary doesn't contains key. This is to improve the performance.
+        
+        let fibResult: UInt64 = (fibDict[n-2]  ?? fibonacciRecurrsive(n-2))
+            &+ (fibDict[n-1] ?? fibonacciRecurrsive(n-1))
+        
+        fibDict[n] = fibResult // Store the result in dictionary
+        
+        fibDict.removeValue(forKey: n - 3) // Remove the key from dictionary that will not be required further. This will avoid consuming memory by keeping unused values in dictionary.
 
         return fibResult
     }
