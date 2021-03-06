391cd06f83a0b577a1a1793438bf5542cc5b2318
“Smriti1006” <smriti.raina1006@gmail.com>
2021-03-06 16:38:48 +0100

    Adding recursion and termination criteria

--- FibonacciOnScreen/ViewController.swift
+++ FibonacciOnScreen/ViewController.swift
@@ -7,46 +7,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextViewDelegate {
 
-    let maxLimit = UInt64.max
-    var fibArray: [UInt64] = []
-    var a = UInt64(0)
-    var b = UInt64(1)
-
+    private var fibDict = [Int: UInt64] ()
+    
     @IBOutlet weak var fibonacciSeriesTextView: UITextView!
     
     //MARK: - View life cycle functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.fibonacciSeriesTextView.text = "\(a),\n"
-        self.fibonacciSequence(UInt64(maxLimit))
-        // Do any additional setup after loading the view.
+
+        self.fibonacciSeriesTextView.delegate = self
+        self.fibonacciSeriesTextView.layoutManager.allowsNonContiguousLayout = false
+        self.getFibSeries()
     }
 
     //MARK: - Private functions
 
-    private func fibonacciSequence(_ n: UInt64){
-
-        fibArray.append(a)
-        fibArray.append(b)
+    private func getFibSeries() {
+        let maxIntValue = UInt64.max // As per requirement its UInt64 max, can be changed as needed
+        var index : Int = 0
+        var result : UInt64 = 0
+        var temp : UInt64 = 0
         
-        guard n > 1 else { return }
         DispatchQueue.global(qos: .background).async { [self] in
-        (2...n).forEach { _ in
-            (a, b) = (a &+ b, a)
-            do{
-                sleep(1)
-            }
-            DispatchQueue.main.sync { [self] in
-                self.fibonacciSeriesTextView.text = self.fibonacciSeriesTextView.text + "\(a),\n"
-            }
-        }
-        }
         
+        while true {
+            
+            result = fibRecur(index)
+            
+            let sum = temp.addingReportingOverflow(result).overflow
+            
+            if sum == true {
+                print("Overflow occurred at",index)
+                self.updateTextViewWith(nextNum: result)
+                break
+            }
+            
+            if result >= maxIntValue {
+                print("Max UInt64 value reached!!")
+                break
+            }
+            
+            self.updateTextViewWith(nextNum: result)
+            
+            index = index + 1
+            temp = result
+            usleep(1000)
+        }
+        }
     }
     
+    private func updateTextViewWith(nextNum: UInt64){
+        DispatchQueue.main.sync { [self] in
+                self.fibonacciSeriesTextView.text = self.fibonacciSeriesTextView.text + "\(nextNum),\n"
+            let range = NSMakeRange(self.fibonacciSeriesTextView.text.count, 0);
+            self.fibonacciSeriesTextView.scrollRangeToVisible(range);
 
+            }
+        }
+    
+    
+    private func fibRecur(_ n: Int) -> (UInt64)  {
+ 
+        guard n > 1 else {
+            return UInt64(n)
+        }
+        let fibResult: UInt64 = (((fibDict[n-2] != nil) ? fibDict[n-2]! : fibRecur(n-2))) &+ (((fibDict[n-1] != nil) ? fibDict[n-1]! : fibRecur(n-1)))
+       
+        fibDict[n] = fibResult
+        fibDict.removeValue(forKey: n - 3)
+
+        return fibResult
+    }
+    
 }
-
